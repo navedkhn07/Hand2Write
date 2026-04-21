3bc2d82af57fc49c7103028a5abab5c348e38128
Sheikh Mohd Naved <navedkhn07@gmail.com>
2026-04-21 12:16:29 +0530

    Update Supabase client URL and anon key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nueuzhjcevkkzuyxykil.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51ZXV6aGpjZXZra3p1eXh5a2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NTA5MjYsImV4cCI6MjA3MDMyNjkyNn0.x7S4fUK37uJpkLgB4mOEteSfJvLHMaaPbjfjFAUfx44'
+const supabaseUrl = 'https://fzrlcyfujcoxtpipovqh.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6cmxjeWZ1amNveHRwaXBvdnFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NzQxMTIsImV4cCI6MjA5MjI1MDExMn0.R-7sqjjXtoIl4_0ywLczhYNnMsjm_2c4OwKLlkOJSKk'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
